c28a2efb23348e750ee6a51b7d16c311e69edc87
prem <145581914+premshravan@users.noreply.github.com>
2024-11-12 16:36:25 +0530

    Update ProductGrid.tsx

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -4,7 +4,7 @@ import { AnimatePresence, motion } from "framer-motion";
 import ProductThumb from "./ProductThumb";
 function ProductGrid({ products }: { products: Product[] }) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4 mb-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 mt-4 mb-6">
       {products.map((product) => {
         return (
           <AnimatePresence key={product._id}>
